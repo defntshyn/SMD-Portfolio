9ba6ea937b0c072e7e7b8484356c072599e24b1d
Christine U Danos <cudanos@joyride.com.ph>
2025-08-24 18:11:36 +0800

    asa

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, HashRouter } from 'react-router-dom'
 import Home from './Pages/Home'
 import Works from './Pages/Works'
 import About from './Pages/About'
@@ -10,14 +10,18 @@ import Iridescence from './other/Iridescence'
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="works" element={<Works />} />
-        <Route path="works/:id" element={<Works />} />
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<Error />} />
-      </Route>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<RootLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="works" element={<Works />} />
+            <Route path="works/:id" element={<Works />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<Error />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     )
   );
 
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/SMD-Portfolio/'
+  base: '/SMD-Portfolio/',
 })
